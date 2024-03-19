c440f9f9e9d6d5e224aef74a75feb315f6f9afec
aixion <aixion1506@gmail.com>
2024-03-19 16:37:54 +0900

    저자, 좋아요, 카테고리 스키마 추가

--- models/schemas/post.js
+++ models/schemas/post.js
@@ -9,8 +9,18 @@ const PostSchema = new Schemas({
         type: String,
         required: true,
     },
-}, {
+    author: {
+        type: String,
+        required: true,
+      },
+      likes: {
+        type: Number,
+        required: true,
+        default: 0
+      },
+      category: String
+    }, {
     timestapms: true,
-})
+});
 
 module.exports = PostSchema;
